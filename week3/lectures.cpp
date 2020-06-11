56560bec11f0e95aa774037b055c7ac2220464ed
nursaadat <saadat.nursultan@nu.edu.kz>
2020-06-11 10:04:46 +0600

    Learned structs and classes

--- week3/lectures.cpp
+++ week3/lectures.cpp
@@ -4,26 +4,66 @@
 #include <iostream>
 using namespace std;
 
-/* Struct */
-struct Lecture{
-    string title;
-    int duration;
-    string author;
+class Route{
+    public: 
+        string GetSource(){ return source; }
+        string GetDestination(){ return destination; }
+        int GetLength(){ return length; }
+        void SetSource( const string& new_source )
+        { 
+            source = new_source;
+            UpdateLength();
+        }
+        void SetDestination( const string& new_destination )
+        { 
+            destination = new_destination;
+            UpdateLength();
+        }
+    private:
+        string source;
+        string destination;
+        int length;
 };
 
-void PrintLecture(const Lecture& lecture)
-{
-    cout << "Lecture: " << lecture.title << ", duration: " << lecture.duration << ", author: " << lecture.author << endl;
-}
+/* Struct */
+// struct LectureTitle{
+//     string author;
+//     string specialization;
+//     string week;
+// };
 
-int
-main()
-{
-    int x = 5;
-    /* Ternary operator */
-    string str = (x % 2 == 0) ? "even" : "odd";
+// struct DetailedLecture{
+//     LectureTitle title;
+//     int duration;
+// };
 
-}
+// struct Lecture{
+//     string title;
+//     int duration;
+//     string author;
+// };
+
+// void PrintLecture(const Lecture& lecture)
+// {
+//     cout << "Lecture: " << lecture.title << ", duration: " << lecture.duration << ", author: " << lecture.author << endl;
+// }
+
+// void PrintCourse(const vector<Lecture>& lectures)
+// {
+//     for (const Lecture& lecture : lectures)
+//     {
+//         PrintLecture(lecture);
+//     }
+// }
+
+// int
+// main()
+// {
+//     int x = 5;
+//     /* Ternary operator */
+//     string str = (x % 2 == 0) ? "even" : "odd";
+
+// }
 
 
 /* Functions */
