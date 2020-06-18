b808bf23223f63c54799f62791740f33f7721cd3
nursaadat <saadat.nursultan@nu.edu.kz>
2020-06-18 18:58:17 +0600

    Finished final project

--- week5/FinalProject.cpp
+++ week5/FinalProject.cpp
@@ -6,42 +6,38 @@
 #include <iomanip>
 using namespace std;
 
-struct Day
-{
-    int value;
-    explicit Day(const int& new_val)
-    {
-        value = new_val;
-    }
-};
-
-struct Month
-{
-    int value;
-    explicit Month(const int& new_val)
-    {
-        value = new_val;
-    }
-};
-
-struct Year
-{
-    int value;
-    explicit Year(const int& new_val)
-    {
-        value = new_val;
-    }
-};
-
 class Date 
 {
     public:
-        Date(Year y, Month m, Day d) 
+        Date ()
         {
-            day = d.value;
-            month = m.value;
-            year = y.value;
+            year = 1;
+            month = 1;
+            day = 1;
         }
+        Date(int y, int m, int d)
+        {
+            year = y;
+
+            /* Check month */
+            if (m < 1 || m > 12)
+            {
+                stringstream ss;
+                ss << "Month value is invalid: " << m;
+                throw invalid_argument(ss.str());
+            }
+            month = m;
+            
+            /* Check day */
+            if (d < 1 || d > 31)
+            {
+                stringstream ss;
+                ss << "Day value is invalid: " << d;
+                throw invalid_argument(ss.str());
+            }
+            day = d;
+        }
+
         int GetYear() const
         {
             return year;
@@ -73,6 +69,111 @@ bool operator<(const Date& lhs, const Date& rhs)
     return lhs.GetYear() < rhs.GetYear();
 }
 
+istream& operator>>(istream& s, Date& date)
+{
+    string date_str;
+    s >> date_str;
+    stringstream ss (date_str);
+     
+    int y, m, d;
+
+    if (ss)
+    {   
+        /* If year is negative is OK */  
+        ss >> y;
+
+        /* Ignore just one '-' */
+        if (ss.peek() == '-')
+        {
+            ss.ignore(1);
+        }
+        else 
+        {
+            stringstream err;
+            err << "Wrong date format: " << date_str;
+            throw invalid_argument(err.str());
+        }
+
+        /* Ignore  '-' or '+' */
+        bool is_neg = false;
+        if (ss.peek() == '-')
+        {
+            is_neg = true;
+            ss.ignore(1);
+        }
+        else if (ss.peek() == '+')
+        {
+            ss.ignore(1);
+        }
+
+        /* Determine if not number then error */
+        if (!isdigit(ss.peek()))
+        {
+            stringstream err;
+            err << "Wrong date format: " << date_str;
+            throw invalid_argument(err.str());
+        }
+        
+        /* Store number */
+        ss >> m;
+        if (is_neg)
+        {
+            m *= -1;
+        }
+
+        /* Ignore just one '-' */
+        if (ss.peek() == '-')
+        {
+            ss.ignore(1);
+        }
+        else 
+        {
+            stringstream err;
+            err << "Wrong date format: " << date_str;
+            throw invalid_argument(err.str());
+        }
+      
+        /* Ignore  '-' or '+' */
+        is_neg = false;
+        if (ss.peek() == '-')
+        {
+            is_neg = true;
+            ss.ignore(1);
+        }
+        else if (ss.peek() == '+')
+        {
+            ss.ignore(1);
+        }
+
+        /* Determine if not number then error */
+        if (!isdigit(ss.peek()))
+        {
+            stringstream err;
+            err << "Wrong date format: " << date_str;
+            throw invalid_argument(err.str());
+        }
+        
+        /* Store number */
+        ss >> d;
+        if (is_neg)
+        {
+            d *= -1;
+        }
+
+        string err;
+        if (ss >> err)
+        {
+            stringstream err;
+            err << "Wrong date format: " << date_str;
+            throw invalid_argument(err.str());
+        }
+    }
+    
+    date = Date(y, m, d);
+    return s;
+
+}
+
 class Database
 {
     public:
@@ -81,24 +182,24 @@ class Database
             date_events[date].insert(event);
         }
 
-        bool DeleteEvent(const Date& date, const string& event)
+        void DeleteEvent(const Date& date, const string& event)
         {
             if (date_events[date].count(event) != 0)
             {
                 date_events[date].erase(event);
-                cout << "Deleted successfully";
+                cout << "Deleted successfully" << endl;
             }
             else
             {
-                cout << "Event not found";
+                cout << "Event not found" << endl;
             }            
         }
 
-        int  DeleteDate(const Date& date)
+        void  DeleteDate(const Date& date)
         {
             int n = date_events[date].size();
             date_events.erase(date);
-            cout << "Deleted " << n << " events";
+            cout << "Deleted " << n << " events" << endl;
         }
 
         void Find(const Date& date) const
@@ -121,8 +222,8 @@ class Database
             {
                 for (const string& str : event.second)
                 {
-                    cout << setw(4) << setfill('0') << event.first.GetYear() << 
-                            setw(2) << setfill('0') << event.first.GetMonth() <<
+                    cout << setw(4) << setfill('0') << event.first.GetYear() << '-' <<
+                            setw(2) << setfill('0') << event.first.GetMonth() << '-' <<
                             setw(2) << setfill('0') << event.first.GetDay() <<
                             " " << str << endl;
                 }
@@ -132,56 +233,69 @@ class Database
         map<Date, set<string>> date_events;
 };
 
-bool HandleDateEntry(Date& date)
-{
-    
-    int d, m, y;
-    return false;
-}
-
 int main() {
     Database db;
-
     string command;
+    // stringstream test("Add 0-1-2 event1\nAdd 1-2-3 event2\nFind 0-1-2\nDel 0-1-2\nPrint\nDel 1-2-3 event2\nDel 1-2-3 event2\n");
+    // stringstream test("Add 0-13-32 event1\n");
+
     while (getline(cin, command)) 
     {
-        string command;
-        cin >> command;
-        Date date;
-        if (command == "Add")
+        try
         {
-            if (HandleDateEntry(date))
+            stringstream ss(command);
+            string todo;
+            ss >> todo;
+            if (todo == "Add")
             {
-
+                Date d;
+                ss >>  d;
+                string event;
+                ss >> event;
+                db.AddEvent(d, event);
             }
-        }
-        else if (command == "Del")
-        {
-            if (HandleDateEntry(date))
+            else if (todo == "Del")
             {
+                Date d;
+                ss >>  d;
+                string event;
+                if (ss.peek() == ' ')
+                {
+                    ss >> event;
+                    db.DeleteEvent(d, event);
+                }
+                else
+                {
+                    db.DeleteDate(d);
+                }
                 
             }
-        }
-        else if (command == "Find")
-        {
-            if (HandleDateEntry(date))
+            else if (todo == "Find")
             {
-                
+                Date d;
+                ss >>  d;
+                db.Find(d);
+            }
+            else if (todo == "Print")
+            {
+                db.Print();
+            }
+            else if (todo.size() != 0)
+            {
+                throw invalid_argument("Unknown command: " + todo);
             }
         }
-        else if (command == "Print")
+        catch(const invalid_argument& e)
         {
-
-        }
-        else if (command.size() == 0)
-        {
-            continue;
-        }
-        else
-        {
-            cout << "Unknown command: " << command << endl;
+            cout << e.what() << endl;
+            return 1;
         }
     }
 
     return 0;
 }
+
+// Add 1-1-1f d1
+// Print
+// ...
+// 0001-01-01 d1
